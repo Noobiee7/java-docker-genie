89d2f878a417da5050830a3969455304db8a9757
Noobiee7 <142085941+Noobiee7@users.noreply.github.com>
2025-08-26 13:33:51 +0530

    Update HomeController.java

--- src/main/java/com/example/mypkg/HomeController.java
+++ src/main/java/com/example/mypkg/HomeController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
-	@GetMapping(value = "/DockerProducts")
-	   public String index() {
-	      return "Products";
-	}
 
+    @GetMapping("/")
+    public String home() {
+        return "Products";
+    }
+
+    @GetMapping("/DockerProducts")
+    public String dockerProducts() {
+        return "Products";
+    }
 }
+
